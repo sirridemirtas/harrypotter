4c1a3d249fd2fc44277706b248a580d59e319556
Sırrı Demirtaş <sirridemirtas@gmail.com>
2023-12-19 18:28:22 +0300

    improved

--- src/betweenness_centrality.py
+++ src/betweenness_centrality.py
@@ -2,3 +2,8 @@ import harry_potter as hp
 import networkx as nx
 
 graph = hp.get_graph()
+
+def get_betweenness_centrality(graph = graph):
+    return nx.betweenness_centrality(graph)
+
+print(get_betweenness_centrality())
