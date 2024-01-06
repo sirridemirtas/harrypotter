5ed69bc29bffffd79784e4b22cd7774ddc83ff67
Sırrı Demirtaş <sirridemirtas@gmail.com>
2024-01-06 21:39:38 +0300

    improvements

--- src/degree_distribution.py
+++ src/degree_distribution.py
@@ -4,15 +4,18 @@ import matplotlib.pyplot as plt
 
 graph = hp.get_graph();
 
-def plot_degree_distribution(G):
-    in_degrees = dict(G.in_degree())  # İç dereceleri al
-    out_degrees = dict(G.out_degree())  # Dış dereceleri al
+def plot_degree_distribution(graph, power_law = False):
+    in_degrees = dict(graph.in_degree())  # İç dereceleri al
+    out_degrees = dict(graph.out_degree())  # Dış dereceleri al
 
-    nodes = G.nodes()
+    nodes = graph.nodes()
 
     in_values = [in_degrees[node] for node in nodes]
     out_values = [out_degrees[node] for node in nodes]
 
+    if power_law:
+        in_values.sort(reverse=True)
+        out_values.sort(reverse=True)
     # Sütun grafik çizimi
     plt.bar(nodes, in_values, alpha=0.7, label='In Degree')
     plt.bar(nodes, out_values, alpha=0.7, label='Out Degree', bottom=in_values)
@@ -20,8 +23,12 @@ def plot_degree_distribution(G):
     plt.xlabel('Düğümler')
     plt.ylabel('Derece')
     plt.legend()
-    plt.title('Harry Potter Karakterlerinin Verdikleri Desteğe Göre Derece Dağılımı')
+    plt.title(
+        "Derece Dağılımı" if power_law==False
+        else "Derece Dağılımı (Güç Yasası)"
+    )
     plt.show()
 
 # Derece dağılımını çiz
 plot_degree_distribution(graph)
+plot_degree_distribution(graph, power_law=True)
--- src/power_law_distrubition.py
+++ src/power_law_distrubition.py
@@ -1,4 +1,36 @@
 import harry_potter as hp
-import networkx as nx
+import matplotlib.pyplot as plt
 
 graph = hp.get_graph()
+
+def plot_power_law_distribution(graph):
+    degrees = dict(graph.degree())
+    nodes = graph.nodes()
+
+    values = [degrees[node] for node in nodes]
+    values.sort(reverse=True)
+
+    # Sütun grafik çizimi
+    plt.bar(nodes, values, alpha=0.7, label='Derece')
+
+    plt.xlabel('Düğümler')
+    plt.ylabel('Derece')
+    plt.legend()
+    plt.title('Güç Yasası Dağılımı')
+    plt.show()
+
+def plot_power_law_distribution_2(graph):
+    degrees = dict(graph.degree())
+    nodes = graph.nodes()
+    values = [degrees[node] for node in nodes]
+    values.sort(reverse=True)
+
+    plt.xscale('log')
+    plt.loglog(values, 'b-', marker='o')
+    plt.xlabel('Düğümler')
+    plt.ylabel('Derece')
+    plt.title('Güç Yasası Dağılımı')
+    plt.show()
+
+plot_power_law_distribution(graph)
+plot_power_law_distribution_2(graph)
