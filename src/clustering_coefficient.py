0d0c27364efa56500e4b909c6038273c5750eab9
Sırrı Demirtaş <sirridemirtas@gmail.com>
2024-01-06 16:18:54 +0300

    add avg clustering coefficent

--- src/clustering_coefficient.py
+++ src/clustering_coefficient.py
@@ -3,8 +3,24 @@ import networkx as nx
 
 graph = hp.get_graph()
 
-# calculate clustering coefficient of grap with function
 def get_clustering_coefficient(graph = graph):
+    """
+    Her düğüm için kümelenme katsayısını dizi olarak döndürür
+    """
     return nx.clustering(graph)
 
-print(get_clustering_coefficient())
+def get_average_clustering_coefficient(graph = graph):
+    """
+    Graf için ortalama kümelenme katsayısını döndürür
+    """
+    return nx.average_clustering(graph)
+
+print(
+    "Her düğüm için ortalama kümelenme katsayısı:",
+    get_clustering_coefficient()
+)
+
+print(
+    "Ortalama kümelenme katsayısı:",
+    get_average_clustering_coefficient()
+)
