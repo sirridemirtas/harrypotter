0305c696e18870fa87cff9cd6d0853084b540412
Sırrı Demirtaş <sirridemirtas@gmail.com>
2024-01-10 17:42:39 +0300

    complated

--- src/assortative_mixing.py
+++ src/assortative_mixing.py
@@ -2,3 +2,18 @@ import harry_potter as hp
 import networkx as nx
 
 graph = hp.get_graph()
+
+print(
+    "Akranlığa göre homofili katsayısı:",
+    nx.attribute_assortativity_coefficient(graph, 'schoolyear')
+)
+
+print(
+    "Cinsiyet bilgisine göre homofili katsayısı:",
+    nx.attribute_assortativity_coefficient(graph, 'gender')
+)
+
+print(
+    "Hane bilgisine göre homofili katsayısı:",
+    nx.attribute_assortativity_coefficient(graph, 'house')
+)
